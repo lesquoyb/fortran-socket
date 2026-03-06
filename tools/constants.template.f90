ca0bd790952167e88f41f8ea42ccc293c156fdc3
Baptiste Lesquoy <lesquoy.baptiste@gmail.com>
2026-03-06 11:07:35 +0700

    restore proper IS_WINDOWS in constants.template.f90

--- tools/constants.template.f90
+++ tools/constants.template.f90
@@ -3,7 +3,11 @@ module socket_lib_constants
     implicit none
 
 
-#define IS_WINDOWS (defined(_WIN32) || defined(__WIN32) || defined(WIN32) || defined(__WIN32__))
+#if defined(_WIN32) || defined(__WIN32) || defined(WIN32) || defined(__WIN32__)
+#define IS_WINDOWS 1
+#else
+#define IS_WINDOWS 0
+#endif
 
 #if IS_WINDOWS
     ! Define socket constants for Windows systems
